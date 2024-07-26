ab5ec713c6c58d7aac83658314b58a4a9de9ba5d
nfmmendes <nilson.fmmendes@outlook.com>
2024-07-26 16:50:36 +0200

    Create AnswerOptionsListView custom component

---
+++ AnswerOptionsListView.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace exam_creator
+{
+    /// <summary>
+    /// Per utilizzare questo controllo personalizzato in un file XAML, eseguire i passaggi 1a o 1b e 2.
+    ///
+    /// Passaggio 1a) Utilizzo di questo controllo personalizzato in un file XAML esistente nel progetto corrente.
+    /// Aggiungere questo attributo XmlNamespace all'elemento radice del file di markup dove 
+    /// deve essere utilizzato:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:exam_creator"
+    ///
+    ///
+    /// Passaggio 1b) Utilizzo di questo controllo personalizzato in un file XAML esistente in un progetto diverso.
+    /// Aggiungere questo attributo XmlNamespace all'elemento radice del file di markup dove 
+    /// deve essere utilizzato:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:exam_creator;assembly=exam_creator"
+    ///
+    /// Sarà inoltre necessario aggiungere nel progetto in cui si trova il file XAML
+    /// un riferimento a questo progetto, quindi ricompilare per evitare errori di compilazione:
+    ///
+    ///     In Esplora soluzioni, fare clic con il pulsante destro del mouse sul progetto di destinazione, quindi scegliere
+    ///     "Aggiungi riferimento"->"Progetti"->[Individuare e selezionare questo progetto]
+    ///
+    ///
+    /// Passaggio 2)
+    /// Utilizzare il controllo nel file XAML.
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public class AnswerOptionsListView : Control
+    {
+        static AnswerOptionsListView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AnswerOptionsListView), new FrameworkPropertyMetadata(typeof(AnswerOptionsListView)));
+        }
+    }
+}
