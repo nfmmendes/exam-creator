15e202047cbcdf9610cd03cc70baccd92c2430c2
nfmmendes <nilson.fmmendes@outlook.com>
2024-07-27 16:56:46 +0200

    Refactor AnswerOptionsListView as user control

--- AnswerOptionsListView.cs
+++ AnswerOptionsListView.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using exam_creator.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,14 +20,21 @@ namespace exam_creator
     /// <summary>
     /// Class <c>AnswerOptionsListView</c> is a control to create a list of alternatives of a question.
     /// </summary>
-    public class AnswerOptionsListView : Control
+    public partial class AnswerOptionsListView : UserControl
     {
+        public List<QuestionAlternative> alternatives { get; private set; } = new List<QuestionAlternative>();
+
         /// <summary>
         /// Static constructor. 
         /// </summary>
-        static AnswerOptionsListView()
+        public AnswerOptionsListView()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(AnswerOptionsListView), new FrameworkPropertyMetadata(typeof(AnswerOptionsListView)));
+            InitializeComponent();
+        }
+
+        void OnBtnAddAlternative_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("OK OK");
         }
     }
 }
