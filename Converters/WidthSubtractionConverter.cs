7cf2b6f5cab5f518abcb4dec3c3d9a22c76d5249
nfmmendes <nilson.fmmendes@outlook.com>
2024-07-27 23:16:34 +0200

    Add WidthSubtractionConverter class

---
+++ Converters/WidthSubtractionConverter.cs
@@ -0,0 +1,27 @@
+﻿using exam_creator;
+using System.Globalization;
+using System.Windows.Data;
+
+
+namespace exam_creator.Converters
+{
+    public class WidthSubtractionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double originalWidth)
+            {
+                if (double.TryParse(parameter.ToString(), out double subtraction))
+                {
+                    return originalWidth - subtraction;
+                }
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
