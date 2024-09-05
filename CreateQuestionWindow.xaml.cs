0765ff9ee94beec4eef714fbc604b1ca92db1579
nfmmendes <nilson.fmmendes@outlook.com>
2024-09-05 16:12:36 +0200

    Implement cancel button click handler

--- CreateQuestionWindow.xaml.cs
+++ CreateQuestionWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using exam_creator.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,28 @@ namespace exam_creator
     /// </summary>
     public partial class CreateQuestionWindow : Window
     {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public CreateQuestionWindow()
         {
             InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Handle the window closing event.
+        /// </summary>
+        /// <param name="sender">The sender object (the window itself)</param>
+        /// <param name="e">The event args object</param>
+        public void OnClosing(object sender, CancelEventArgs e)
+        {
+            if (answerListCreator.Alternatives.Count > 0 || questionText.Text.Length > 0)
+            {
+                var message = "Are you sure you want to close this window? You have unsaved changes that will be lost.";
+                if (MessageBox.Show(message, "Warning", MessageBoxButton.YesNo) == MessageBoxResult.No)
+                    e.Cancel = true;
+            }
         }
     }
 }
